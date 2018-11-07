83423dcc5c0b485ca60c99b9136943e91610017e
shuaida <111111@qq.com>
2018-11-07 17:23:05 +0800

    初始化

---
+++ src/router/index.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+// 导入login
+import Login from '@/components/login.vue'
+
+Vue.use(Router)
+export default new Router({
+  routes: [
+    {path: '/', redirect: '/login'},
+    {path: '/login', component: Login}
+  ]
+})
